4008fd488453bb187807c18a15585d12fbca0c33
s.deninno <s.deninno@namirial.com>
2020-05-25 13:15:27 +0200

    start

---
+++ StopWatch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StopWatch
+{
+   public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _stopTime;
+
+        private bool _running; //di default è inizializzato come falso;
+
+        public void Start()
+        {
+            if (_running)
+            { throw new InvalidOperationException("It's already running"); }
+            
+            _startTime = DateTime.Now;
+            _running = true;
+       
+        }
+
+        public void Stop()
+        {
+            if (!_running)
+            { throw new InvalidOperationException("It's not running"); }
+           
+            _stopTime = DateTime.Now;
+            _running = false;
+
+        }
+
+        public TimeSpan GetTimeSpan()
+        {
+            return _stopTime - _startTime;
+
+        }
+
+    }
+}
+    
+
