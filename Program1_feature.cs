4ee7291d60fef276902578581e69787c0089ce9c
s.deninno <s.deninno@namirial.com>
2020-05-25 14:30:32 +0200

    feature1.0

---
+++ Program1_feature.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+
+    class Program_1_feat
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new StopWatch();
+
+            for (var i=0; i<2; i++) 
+            {
+                stopwatch.Start();
+
+                //Thread.Sleep(1000); //ritardo di 1000 millisecondi all'applicazione
+
+                stopwatch.Stop();
+
+                Console.WriteLine("Duration: " + stopwatch.GetTimeSpan());
+            Console.ReadLine();
+            }
+          
+
+
+
+        }
+    }
+}
+
